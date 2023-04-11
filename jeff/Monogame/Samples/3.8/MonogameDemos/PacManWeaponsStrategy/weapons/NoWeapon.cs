b3a17a05cc85b0ac657166f2b865959a476852cf
Jeff Meyers <jmeyers@colum.edu>
2023-04-11 14:08:54 -0500

    add pacmanweapon stratedgy

---
+++ jeff/Monogame/Samples/3.8/MonogameDemos/PacManWeaponsStrategy/weapons/NoWeapon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using Microsoft.Xna.Framework.Content;
+using MonoGameLibrary.Sprite;
+using PacManWeaponsStrategy;
+
+namespace StrategyPacMan.weapons
+{
+    class NoWeapon : foodWeapon
+    {
+        public NoWeapon(Game game)
+            : base(game)
+        {
+            this.Name = "no weapon";
+            this.Verb = "no verb";
+            this.color = Color.White;
+        }
+
+        public override string Use()
+        {
+            return string.Empty; //base.Use();
+        }
+    }
+}
---
+++ jeff/Monogame/Samples/3.8/MonogameDemos/PacManWeaponsStrategy/weapons/TealWeapon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using Microsoft.Xna.Framework.Content;
+using MonoGameLibrary.Sprite;
+using PacManWeaponsStrategy;
+
+namespace StrategyPacMan.weapons
+{
+    class TealWeapon : foodWeapon
+    {
+        public TealWeapon(Game game)
+            : base(game)
+        {
+            this.Name = "teal weapon";
+            this.Verb = "teal chomp";
+            this.color = Color.Teal;
+        }
+    }
+}
